5c2da282b2a6196bac29eb9a8b4c3e60df653395
kairodeveloper <kairocosta98@gmail.com>
2017-08-07 11:39:01 -0300

    Ambiente inicial configurado com virtualenv e postgreesql

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kairo_costa/Documentos/Projetos/Projeto_Eventos_Wednesday/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
