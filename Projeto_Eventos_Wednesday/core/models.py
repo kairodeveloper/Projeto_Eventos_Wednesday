6dac93958b708367a9965e837d1ab10b1db4f597
Guilherme2020 <guilhermerodrigues123477@gmail.com>
2017-08-16 20:30:15 -0300

    inserindo metodos na classe cupom e na classe Evento

--- Projeto_Eventos_Wednesday/core/models.py
+++ Projeto_Eventos_Wednesday/core/models.py
@@ -50,6 +50,12 @@ class Atividade(models.Model):
     data = models.DateField()
     tipo_atividade = EnumField(TipoAtividade, default=TipoAtividade.DEFAULT)
 
+class Usuario(models.Model):
+    cod_user = models.IntegerField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=9)
+    email = models.CharField(max_length=45)
+    senha = models.CharField(max_length=20)
 
 class Evento(models.Model):
     cod_evento = models.IntegerField(primary_key=True)
@@ -62,6 +68,31 @@ class Evento(models.Model):
     estado_evento = EnumField(EstadoEvento, default=EstadoEvento.DEFAULT)
     atividades = models.ForeignKey(Atividade,on_delete=models.CASCADE)
 
+    valor_evento = models.DecimalField()
+    def __init___(self,titulo,cod_evento):
+        self._titulo = titulo
+        self._cod_evento = cod_evento
+
+    def __str__(self):
+        return self.titulo
+    @property
+    def titulo(self):
+        print("Titulo do Evento: ")
+        return self._titulo
+
+    @property
+    def atividades(self):
+        return self.atividades.all()
+
+    @property
+    def administrador(self):
+        return self.administrador
+    def adicionar_atividade_evento(self,atividades):
+        pass
+
+    def validar_data_evento(self,data_inicio,data_fim):
+        if data_fim.date() < data_inicio.date():
+            raise Exception("Data invalida ")
 
 class Cupom(models.Model):
     cod_cupom = models.IntegerField(primary_key=True)
@@ -69,8 +100,20 @@ class Cupom(models.Model):
     validade = models.DateField()
     evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
 
-    def __init__(self):
-        pass
+    def __init__(self,nome_cupom, desconto):
+        self._desconto = desconto
+        self._nome_cupom = nome_cupom
+
+    @property
+    def desconto(self):
+        return self._desconto
+    @desconto.setter
+    def desconto(self,valor_desconto):
+        if not  isinstance(int,float):
+            raise ValueError("Valor de desconto invalido")
+        self._desconto = valor_desconto
+    def calcular_valor_cupom(self,valor_evento):
+        return self._desconto * valor_evento
 
 class Instituicao(models.Model):
     cod_instituicao = models.IntegerField(primary_key=True)
@@ -88,3 +131,18 @@ class Apoio(models.Model):
 class Pagamento(models.Model):
     datapagamento = models.DateField()
     status = EnumField(EstadoInscricao, default=EstadoInscricao.NAO_PAGO)
+
+class Inscricao(models.Model):
+    cod_inscricao = models.IntegerField(primary_key=True)
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
+    solicitante = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    data_inscricao = models.DateTimeField()
+    valor = models.FloatField()
+    estado_inscricao = EnumField(EstadoInscricao, default=EstadoInscricao.NAO_PAGO)
+    dia_pagamento = models.DateField()
+
+class Item_Inscricao(models.Model):
+    cod_item = models.IntegerField(primary_key=True)
+    inscricao = models.ForeignKey(Inscricao, on_delete=models.CASCADE)
+    atividade = models.ForeignKey(Atividade, on_delete=models.CASCADE)
+    horario = models.TimeField()
